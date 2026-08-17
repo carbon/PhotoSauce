3eb3a70abf1bb5a96ac1b69d14b13588a64799ae
myluma <claudee@mymind.com>
2026-08-17 01:46:09 -0700

    Fix libde265 build and update Libheif package version

--- build/vcpkg/ports/libde265/portfile.cmake
+++ build/vcpkg/ports/libde265/portfile.cmake
@@ -5,7 +5,8 @@ vcpkg_from_github(
     SHA512 fb2207f5a3ba901853f61f345c72130f000134918febbc4f3529c3d289fc79ee7457b3e61660110f698bb4ac15d62426e284034bf870bfbd1859ab3feaa52be8
     HEAD_REF master
     PATCHES
-        fix-interface-include.patch
+        fix-linkage.patch
+        fix-api-visibility.patch
 )
 
 vcpkg_cmake_configure(
@@ -22,9 +23,9 @@ vcpkg_fixup_pkgconfig()
 
 if(VCPKG_LIBRARY_LINKAGE STREQUAL "static")
     file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/bin" "${CURRENT_PACKAGES_DIR}/debug/bin")
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/libde265/de265.h" "!defined(LIBDE265_STATIC_BUILD)" "0")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/libde265/de265.h" "defined(LIBDE265_STATIC_BUILD)" "1")
 else()
-    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/libde265/de265.h" "!defined(LIBDE265_STATIC_BUILD)" "1")
+    vcpkg_replace_string("${CURRENT_PACKAGES_DIR}/include/libde265/de265.h" "defined(LIBDE265_STATIC_BUILD)" "0")
 endif()
 
 file(REMOVE_RECURSE "${CURRENT_PACKAGES_DIR}/debug/include")
